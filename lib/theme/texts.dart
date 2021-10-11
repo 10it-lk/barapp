e9259794023ae6436c5f246892cf13b889f8c0e4
Pathi De F <pathideva@10it.lk>
2021-10-11 10:24:18 +0530

    added theme files

---
+++ lib/theme/texts.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:bar_app/theme/colors.dart';
+
+///
+/// Primary Font Styles (Open Sans)
+///
+// ignore: unnecessary_const
+const primaryH1Bold = const TextStyle(
+    color: DarwinPrimaryDark,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Open Sans',
+    fontStyle: FontStyle.normal,
+    fontSize: 37.0);
+
+// ignore: unnecessary_const
+const primaryH1Regular = const TextStyle(
+    color: DarwinPrimaryDark,
+    fontWeight: FontWeight.w300,
+    fontFamily: 'Open Sans',
+    fontStyle: FontStyle.normal,
+    fontSize: 37.0);
+
+const primaryPRegular = TextStyle(
+    color: DarwinPrimaryDark,
+    fontWeight: FontWeight.w300,
+    fontFamily: 'Open Sans',
+    fontStyle: FontStyle.normal,
+    fontSize: 16.0);
+
+const primaryPBold = TextStyle(
+    color: DarwinPrimaryDark,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Open Sans',
+    fontStyle: FontStyle.normal,
+    fontSize: 16.0);
+
+///
+/// Secondary Font Styles (Roboto)
+///
+const secondaryH1Bold = TextStyle(
+    color: DarwinPrimaryDark,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Roboto',
+    fontStyle: FontStyle.normal,
+    fontSize: 37.0);
+
+const secondaryH1Regular = TextStyle(
+    color: DarwinPrimaryDark,
+    fontWeight: FontWeight.w300,
+    fontFamily: 'Roboto',
+    fontStyle: FontStyle.normal,
+    fontSize: 37.0);
+
+const secondaryPRegular = TextStyle(
+    color: DarwinPrimaryDark,
+    fontWeight: FontWeight.w300,
+    fontFamily: 'Roboto',
+    fontStyle: FontStyle.normal,
+    fontSize: 16.0);
+
+const secondaryPBold = TextStyle(
+    color: DarwinPrimaryDark,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Roboto',
+    fontStyle: FontStyle.normal,
+    fontSize: 16.0);
