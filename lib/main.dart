e5e84859f1a698dcd4fd000fd90dd75375767411
Pathi De F <pathideva@10it.lk>
2021-10-26 21:45:26 +0530

    sizer

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:bar_app/routes/route.dart' as route;
 import 'package:bar_app/screens/bar_menu.dart';
+import 'package:sizer/sizer.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,16 +11,33 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
+  // @override
+  // Widget build(BuildContext context) {
+  //   return MaterialApp(
+  //     title: 'Bar App',
+  //     debugShowCheckedModeBanner: false,
+  //     theme: ThemeData(
+  //       primarySwatch: Colors.blue,
+  //     ),
+  //     onGenerateRoute: route.controller,
+  //     home: BarMenuPage(),
+  //   );
+  // }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Bar App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      onGenerateRoute: route.controller,
-      home: BarMenuPage(),
+    return Sizer(
+      builder: (context, orientation, deviceType) {
+        return MaterialApp(
+          title: 'Bar App',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          onGenerateRoute: route.controller,
+          home: BarMenuPage(),
+        );
+      },
     );
   }
 }
--- lib/screens/bar_menu.dart
+++ lib/screens/bar_menu.dart
@@ -1,5 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:bar_app/routes/route.dart' as route;
+import 'package:bar_app/theme/colors.dart' as color;
+import 'package:sizer/sizer.dart';
+import 'package:auto_size_text_pk/auto_size_text_pk.dart';
+
+List<GridListItems> items = [
+  GridListItems(color.shot1, 'Cheap tequila', Icons.looks_one_outlined),
+  GridListItems(
+      color.shot2, 'Pineapple Upside Down Cake', Icons.looks_one_outlined),
+  GridListItems(color.shot3, 'Prairie Oyster', Icons.looks_one_outlined),
+  GridListItems(color.shot4, 'Hot Damn', Icons.looks_one_outlined),
+  GridListItems(color.shot5, 'B - 52', Icons.looks_one_outlined),
+  GridListItems(color.shot6, 'Alice In Wonderland', Icons.looks_one_outlined),
+  GridListItems(color.shot7, 'Jolly Rancher', Icons.looks_one_outlined),
+  GridListItems(color.shot8, 'Mind Eraser', Icons.looks_one_outlined),
+  GridListItems(color.shot9, 'Motor Oil', Icons.looks_one_outlined),
+  GridListItems(color.shot10, 'Afterburner', Icons.looks_one_outlined),
+  GridListItems(color.shot11, 'Irish Car Bomb', Icons.looks_one_outlined),
+  GridListItems(color.shot12, 'Kamikaze', Icons.looks_one_outlined),
+];
+
+class GridListItems {
+  Color color;
+  String title;
+  IconData icon;
+  GridListItems(this.color, this.title, this.icon);
+}
+
+// class RandomColorModel {
+//   Random random = Random();
+//   Color getColor() {
+//     return Color.fromARGB(random.nextInt(300), random.nextInt(300),
+//         random.nextInt(300), random.nextInt(300));
+//   }
+// }
 
 // ignore: use_key_in_widget_constructors
 class BarMenuPage extends StatelessWidget {
@@ -9,16 +43,53 @@ class BarMenuPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         // ignore: prefer_const_constructors
-        title: Text('Menu Page'),
+        title: Text('Menu'),
       ),
       // ignore: prefer_const_constructors
       body: Center(
         // code for the button in the home page
         // ignore: prefer_const_constructors
-        child: ElevatedButton(
-          // ignore: prefer_const_constructors
-          child: Text('Go to Recipe'),
-          onPressed: () => Navigator.pushNamed(context, route.recipePage),
+        child: GridView.count(
+          crossAxisCount: 4,
+          childAspectRatio: (2 / 1),
+          crossAxisSpacing: 5,
+          mainAxisSpacing: 5,
+          //physics:BouncingScrollPhysics(),
+          padding: EdgeInsets.all(10.0),
+          children: items
+              .map(
+                (data) => GestureDetector(
+                    onTap: () => Navigator.pushNamed(context, route.recipePage),
+                    child: Container(
+                      padding: const EdgeInsets.all(8),
+                      //color: data.color,
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.greenAccent)),
+                      child: Column(
+                        children: [
+                          Icon(
+                            data.icon,
+                            size: 48,
+                            color: Colors.black,
+                          ),
+                          Expanded(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.blueAccent)),
+                              child: AutoSizeText(
+                                data.title,
+                                textAlign: TextAlign.center,
+                                maxLines: 2,
+                                //style: const TextStyle(fontSize: 36)
+                                style: TextStyle(fontSize: 10.sp),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    )),
+              )
+              .toList(),
         ),
       ),
     );
